bb1e8bbca424e407889eaa383719cf2032536b94
shiyang07ca <shiyang07ca@gmail.com>
2023-05-29 11:55:13 +0800

    binary search

--- 1.py
+++ 1.py
@@ -1,3 +1,17 @@
+def binary_search(arr, target):
+    l, r = 0, len(arr) - 1
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == target:
+            return mid
+        if arr[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+
+    return None
+
+
 def main():
     pass
 
